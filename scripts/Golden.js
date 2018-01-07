6a3d7529576d70cc91c0bf8742545242f6d478d5
Nereikia <vilius.somka@gmail.com>
2018-01-07 18:11:22 +0200

    update

--- scripts/Golden.js
+++ scripts/Golden.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
-    $(".burger-container").on('click', function(){
-        $(this).toggleClass("open");
-    });
+$(document).on('scroll', function() {
+  if($(window).scrollTop()> 300) {
+    $('.top').fadeIn();
+  }
+  else {
+    $('.top').fadeOut();
+  }
+});
+
+$('.top').on('click', function() {
+   $("html, body").animate({ scrollTop: 0 }, "slow");
 });
